cf97a1caf21d447668a97b9697c95ef98547a0cc
Joe Berria <joeberria@gmail.com>
2023-08-15 11:26:05 -0700

    adding Chip8 roms to run/test with

--- src/main.rs
+++ src/main.rs
@@ -4,6 +4,6 @@ fn main() -> Result<(), String> {
     let display = Display::create("Chip-8".to_string(), 10);
     let mut system = System::create(display);
 
-    system.load_rom("IBM_Logo.ch8")?;
+    system.load_rom("roms/IBM_Logo.ch8")?;
     system.run()
 }
